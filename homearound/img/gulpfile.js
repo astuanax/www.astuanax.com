a1936bfaca6417f36d781977ebaf64f704349ce6
Len Dierickx <lendierickx@Len-Dierickxs-iMac.fritz.box>
2016-07-21 19:14:24 +0200

    optimisation

---
+++ homearound/img/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+var minify = require('gulp-minify');
+
+
+gulp.task('minify', function() {
+  return gulp.src('./_site/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('_site'))
+});
+
+
+gulp.task('optimise', () =>
+    gulp.src('./_site/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./_site/img'))
+);
+
+
+gulp.task('compress', function() {
+  gulp.src('./_site/js/*.js')
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.js'
+        },
+        ignoreFiles: ['min.js']
+    }))
+    .pipe(gulp.dest('./_site/js'))
+});
